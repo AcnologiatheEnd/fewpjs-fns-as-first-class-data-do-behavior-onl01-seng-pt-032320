a2904828fb501006e5b62b5f7d5d9c29a0b9682d
Winston  Chung <18.chung.winston@ctkprep.org>
2020-09-21 14:27:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 /* Given Code, don't edit */
 
-document.querySelector("button").addEventListener("click", handleClick);
-
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
@@ -10,4 +8,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(s){
+    let string = s.split(" ");
+    let p = parseInt(string);
+    if (p < 12){
+    return "Good Morning";
+    } else if (p > 12 && p  < 17){
+    return "Good Afternoon";
+    } else {
+    return "Good Evening";
+    }
+};
 /* Write your implementation of displayMessage() */
+function displayMessage(s) {
+  let string = document.getElementById("greeting");
+  string.innerText = s;
+} 
